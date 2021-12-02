9761d774b549dc42fe4f428dd8516fe7c9ac4866
schef <zvanstefan@gmail.com>
2021-12-02 17:02:16 +0100

    added i2c

--- README.md
+++ README.md
@@ -4,8 +4,23 @@ sudo pacman -S python-paho-mqtt
 mosquitto_sub -h HOST -p PORT -u USER -P PASS -v -t "#"
 mosquitto_pub -h HOST -p PORT -u USER -P PASS -t 'device_outdoor/input/light' -m '0'
 
+sudo pacman -S python-periphery
+
+https://ozzmaker.com/i2c/
+sudo pacman -S i2c-tools
+/etc/modules-load.d/raspberry_i2c.conf
+i2c-dev
+i2c-bcm2708
+
+/boot/config.txt
+dtparam=i2c_arm=on
+dtparam=i2c1=on
+
 groupadd gpio
 usermod -a -G gpio alarm
 
 /etc/udev/rules.d/10-alarm-gpio.rules
 SUBSYSTEM=="gpio", ACTION=="add", PROGRAM="/bin/sh -c 'chgrp -R gpio /dev/gpiochip* && chmod -R g+rw /dev/gpiochip*'"
+
+/etc/udev/rules.d/10-alarm-i2c.rules
+SUBSYSTEM=="i2c-dev", ACTION=="add", PROGRAM="/bin/sh -c 'chgrp -R gpio /dev/i2c* && chmod -R g+rw /dev/i2c*'"
--- src/rpi_peripherals.py
+++ src/rpi_peripherals.py
@@ -1,4 +1,5 @@
-from periphery import GPIO
+from periphery import GPIO, I2C
+import ina219
 
 # https://cdn.sparkfun.com/assets/learn_tutorials/6/7/6/PiZero_1.pdf
 
@@ -8,10 +9,15 @@ RELAY_1 = 18
 RELAY_2 = 23
 RELAY_3 = 24
 RELAY_4 = 25
+SHUNT_OHMS = 0.1
 
 button_led = None
 button_in = None
 relays = []
+i2c = None
+ina = None
+
+# i2cdetect -y 1
 
 
 def set_button_led(state):
@@ -21,8 +27,20 @@ def set_button_led(state):
 def get_button_state():
     return int(button_in.read())
 
+
 def set_relay(num, state):
     relays[num].write(bool(state))
+
+
+def get_measure():
+    voltage = ina.voltage()
+    current = ina.current()
+    power = ina.power()
+    print("Bus Voltage: %.3f V" % voltage)
+    print("Current: %.3f mA" % current)
+    print("Power: %.3f mW" % power)
+    return (voltage, current, power)
+
 
 if __name__ == "__main__":
     button_led = GPIO("/dev/gpiochip0", BUTTON_LED, "out")
@@ -34,6 +52,10 @@ if __name__ == "__main__":
     relays.append(GPIO("/dev/gpiochip0", RELAY_4, "out"))
     for i in range(len(relays)):
         set_relay(i, False)
+    i2c = I2C("/dev/i2c-1")
+    ina = ina219.INA219(SHUNT_OHMS, i2c)
+    ina.configure()
+
 
     # gpio_in.close()
     # gpio_out.close()
