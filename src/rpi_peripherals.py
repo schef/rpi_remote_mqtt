8e614165e3c7409fc2cb7a6b1a62b8de3420f93c
schef <zvanstefan@gmail.com>
2021-12-02 14:33:35 +0100

    gpiozero

--- src/rpi_peripherals.py
+++ src/rpi_peripherals.py
@@ -1,19 +1,13 @@
-from periphery import GPIO
+from gpiozero import LED
 
 led = None
 
-
 def set_led(state):
-    led.write(state)
+    if state:
+        led.on()
+    else:
+        led.off()
 
 
 if __name__ == "__main__":
-    # Open GPIO /dev/gpiochip0 line 10 with input direction
-    # gpio_in = GPIO("/dev/gpiochip0", 10, "in")
-    # Open GPIO /dev/gpiochip0 line 12 with output direction
-    led = GPIO("/dev/gpiochip0", 26, "out")
-
-    # value = gpio_in.read()
-
-    # gpio_in.close()
-    # gpio_out.close()
+    led = LED(26)
