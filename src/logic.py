b03237be383299b5be89020d66c99f1c733ac5cc
schef <zvanstefan@gmail.com>
2021-12-07 11:30:32 +0100

    writing logic

--- src/logic.py
+++ src/logic.py
@@ -5,6 +5,8 @@ import log
 logger = log.get()
 agregator_state = False
 agregator_in_progress = False
+agregator_step = 0
+timestamp = 0
 
 
 def on_button_state_change(state):
@@ -28,15 +30,35 @@ def init():
 
 
 def check_for_agregator():
-    global agregator_in_progress
-    if agregator_state and agregator_in_progress:
-        rpi_peripherals.set_button_led(1)
-        rpi_peripherals.set_relay(0, 1)
-        agregator_in_progress = False
-    if not agregator_state and agregator_in_progress:
-        rpi_peripherals.set_button_led(0)
-        rpi_peripherals.set_relay(0, 0)
-        agregator_in_progress = False
+    global agregator_in_progress, agregator_step, timestamp
+    if agregator_in_progress:
+        if agregator_state:
+            if agregator_step == 0:
+                logger.info("[LGC]: agregator 0")
+                timestamp = common.get_millis()
+                rpi_peripherals.set_button_led(1)
+                rpi_peripherals.set_relay(0, 1)
+                agregator_step += 1
+            elif agregator_step == 1 and common.millis_passed(timestamp) > 5000:
+                logger.info("[LGC]: agregator 1")
+                timestamp = common.get_millis()
+                rpi_peripherals.set_relay(1, 1)
+                agregator_step += 1
+            elif agregator_step == 2 and common.millis_passed(timestamp) > 5000:
+                logger.info("[LGC]: agregator 2")
+                timestamp = common.get_millis()
+                rpi_peripherals.set_relay(1, 0)
+                rpi_peripherals.set_relay(2, 1)
+                agregator_step += 1
+            elif agregator_step == 3 and common.millis_passed(timestamp) > 5000:
+                logger.info("[LGC]: agregator 3")
+                timestamp = 0
+                rpi_peripherals.set_relay(2, 0)
+                agregator_in_progress = False
+        if not agregator_state:
+            rpi_peripherals.set_button_led(0)
+            rpi_peripherals.set_relay(0, 0)
+            agregator_in_progress = False
 
 
 def loop():
