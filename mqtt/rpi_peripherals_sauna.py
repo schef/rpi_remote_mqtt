24325b8dcebc59a490fe53496d034448a8f9e388
Stjepan Horvat <zvanstefan@gmail.com>
2023-03-09 20:59:25 +0000

    fixed driver

--- mqtt/rpi_peripherals_sauna.py
+++ mqtt/rpi_peripherals_sauna.py
@@ -12,52 +12,8 @@ from credentials import test
 
 RELAY_PINS = [23, 24, 25, 8]
 relays = []
-
-TEMP_0 = '28-0516843756ff'
-TEMP_1 = '28-3c71f6494dd2'
-
-
-class GPIOTest:
-    def __init__(self, pin):
-        self.pin = pin
-
-    def write(self, state):
-        logger.debug(f"TestRelay[{self.pin}].write {state}")
-
-
-class Relay:
-    def __init__(self, pin: int, invert=False):
-        if test:
-            self.relay = GPIOTest(pin)
-        else:
-            self.relay = GPIO("/dev/gpiochip0", pin, "out")
-        self.state = None
-        self.invert = invert
-
-    def set(self, state):
-        if self.invert:
-            self.relay.write(not bool(state))
-        else:
-            self.relay.write(bool(state))
-        self.state = state
-
-    def get(self):
-        return self.state
-
-
-def get_temperature(num):
-    if test:
-        return 23.456
-    else:
-        try:
-            if num == 0:
-                return ds18b20.read_temp(TEMP_0)
-            elif num == 1:
-                return ds18b20.read_temp(TEMP_1)
-            elif num == 2:
-                return ds18b20.read_temp(TEMP_2)
-        except:
-            return None
+TEMPERATURE_ADDRESSES = ['28-0516843756ff', '28-3c71f6494dd2']
+temperature_sensors = []
 
 
 def init():
